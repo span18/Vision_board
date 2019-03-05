0defe2895756bac5fbd5de3d98dc925938429db1
daniel <dmjames04@yahoo.com>
2019-03-05 11:40:33 +0000

    updates

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
-console.log("hello");
+ function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
