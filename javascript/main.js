0c3365f99759a1fbbf8567aa102a7d8833df1a28
daniel <dmjames04@yahoo.com>
2019-03-05 20:29:01 +0000

    update to js

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,16 @@
- function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
+/* Upload image */
 
-                reader.onload = function (e) {
-                    $('#blah')
-                        .attr('src', e.target.result);
-                };
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
