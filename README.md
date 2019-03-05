22847f583c53f07fbb3629e90db26c05cf0dbc38
daniel <dmjames04@yahoo.com>
2019-03-05 20:40:49 +0000

    update to notes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Vision_board
+
+
+Checklist
+
+User will be able to upload photos
+User will be able to add paragraphs to the photo
+User will be able to move the photo
--- javascript/main.js
+++ javascript/main.js
@@ -7,8 +7,6 @@ function readURL(input) {
         reader.onload = function (e) {
             $('#blah')
                 .attr('src', e.target.result)
-                .width(150)
-                .height(200);
         };
 
         reader.readAsDataURL(input.files[0]);
