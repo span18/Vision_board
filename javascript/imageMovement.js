c6b033de7b48ee2def46323e224ee9567ecc49cc
daniel <dmjames04@yahoo.com>
2019-03-13 12:03:31 +0000

    updates

--- javascript/imageMovement.js
+++ javascript/imageMovement.js
@@ -1,43 +1,47 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("move"));
+// Learnt and adapted from: http://tech.pro/tutorial/650/javascript-draggable-elements from a user comments post/code (@TheDesigner in 2009-ish).
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementByIde(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+// Global variables without any specified type (type will be determined when used in the functions below).
+var obj, x, y, prev_x, prev_y;
+
+function drag(e) {
+  // Yep, use the object I just clicked on.
+  obj = e.target;
+  // Set current X coordinate minus distance left from offsetParent node.
+  prev_x = x - obj.offsetLeft;
+  // Set current Y coordinate minus distance top from offsetParent node.
+  prev_y = y - obj.offsetTop;
+  // Change the object's color so it looks like it's usable/moveable.
+  obj.style.background = '#00ff00';
+}
+
+function move(e) {
+  // Always track and record the mouse's current position.
+  if (e.pageX) {
+    x = e.pageX; // X coordinate based on page, not viewport.
+    y = e.pageY; // Y coordinate based on page, not viewport.
   }
+//  else if (e.clientX) {
+//    x=clientX; // X coordinate based on viewport.
+//    y=clientY; // Y coordinate based on viewport.
+//  }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
+  // If the object specifically is selected, then move it to the X/Y coordinates that are always being tracked.
+  if(obj) {
+    obj.style.left = (x - prev_x) + 'px';
+    obj.style.top = (y - prev_y) + 'px';
   }
 }
+
+function drop() {
+  // Revert to the default css style.
+  obj.style.background = '';
+  // Remove the attached event from the element so it doesn't keep following your mouse. :)
+  obj = false;
+}
+
+// Make a specific element movable
+document.getElementById('preview').onmousedown = drag;
+document.onmousemove = move;
+document.onmouseup = drop;
+
+
